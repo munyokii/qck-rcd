64963e030c31a18586f04ed35ee49af9912d098c
munyokii <munyoki912@gmail.com>
2026-02-03 19:19:41 +0300

    Icon displayed correction

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -1,4 +1,3 @@
-// components/ThemeToggle.tsx
 "use client";
 
 import { useTheme } from "next-themes";
@@ -9,18 +8,15 @@ export default function ThemeToggle() {
 
   return (
     <label className="swap swap-rotate">
-      {/* Hidden checkbox controls the state */}
       <input 
         type="checkbox" 
         onChange={() => setTheme(theme === "bumblebee" ? "halloween" : "bumblebee")}
         checked={theme === "halloween"} 
       />
 
-      {/* Sun Icon (Visible when theme is bumblebee/light) */}
-      <Sun className="swap-off h-6 w-6 fill-current" />
+      <Sun className="swap-on h-6 w-6 fill-current" />
 
-      {/* Moon Icon (Visible when theme is halloween/dark) */}
-      <Moon className="swap-on h-6 w-6 fill-current" />
+      <Moon className="swap-off h-6 w-6 fill-current" />
     </label>
   );
 }
