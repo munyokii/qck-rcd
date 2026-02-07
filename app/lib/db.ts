41051ddddcf320b3f8618910ab87aa050f61c300
munyokii <munyoki912@gmail.com>
2026-02-07 16:25:30 +0300

    fix: deprecation warning from the pg driver

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL
-const adapter = new PrismaPg({ connectionString })
+const adapter = new PrismaPg({ 
+  connectionString,
+  ssl: { rejectUnauthorized: false } 
+})
 
 export const prisma = new PrismaClient({adapter})
